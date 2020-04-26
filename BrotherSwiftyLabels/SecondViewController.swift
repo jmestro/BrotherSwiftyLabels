698c2a35dac15f5918b332e9f23293eb66eb09bc
Mizmovac, Inc <joe@mizmovac.com>
2020-04-26 14:31:55 -0700

    Initial Commit

---
+++ BrotherSwiftyLabels/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BrotherSwiftyLabels
+//
+//  Created by Joe Mestrovich on 4/26/20.
+//  Copyright © 2020 Mizmovac. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
